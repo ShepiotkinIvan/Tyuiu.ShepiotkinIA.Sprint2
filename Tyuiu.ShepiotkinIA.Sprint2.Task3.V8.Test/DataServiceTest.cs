44b7060d872f81497eef1270a44c9b8b5087f3e5
Tyuiu.ShepiotkinIA@outlook.com <Tyuiu.ShepiotkinIA@outlook.com>
2023-10-06 21:07:33 +0500

    Task3

---
+++ Tyuiu.ShepiotkinIA.Sprint2.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShepiotkinIA.Sprint2.Task3.V8.Lib;
+
+namespace Tyuiu.ShepiotkinIA.Sprint2.Task3.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = -4.588;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -3;
+            double res = ds.Calculate(x);
+            double wait = 0.422;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -13;
+            double res = ds.Calculate(x);
+            double wait = -142.615;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
